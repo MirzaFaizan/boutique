f40e192adedfe586c2dab70850c68fa92754a86d
zaki abbasi chotu lapi <zakiabbasi15@yahoo.com>
2018-04-24 18:15:58 +0500

    Sales Schema modified

--- models/shopinventory.js
+++ models/shopinventory.js
@@ -5,7 +5,6 @@ var Schema = mongoose.Schema;
 var ShopInventorySchema = new Schema(
   {
     item_id: {type: String,  max: 100},
-    num_stock: {type: Number,  max: 100},
     shop_id: {type: String, required: true, max: 100},
   }
 
