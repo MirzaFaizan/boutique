993667cb29296902b4ada13cd3e3f84fbe308828
Faizan <mirzafaizanejaz@gmail.com>
2018-07-31 11:32:25 +0500

    added search by category

--- client/src/headoffice/ExtraFeatures/StockbyCategory.jsx
+++ client/src/headoffice/ExtraFeatures/StockbyCategory.jsx
@@ -4,6 +4,8 @@ import { withStyles } from 'material-ui/styles';
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
+import { Button } from 'material-ui';
 
 const CustomTableCell = withStyles(theme => ({
   head: {
@@ -14,6 +16,35 @@ const CustomTableCell = withStyles(theme => ({
     fontSize: 14,
   },
 }))(TableCell);
+
+
+const dropdowntypes = [
+  {
+    value: 'Men Clothing',
+    label: 'Men Clothing',
+  },
+  {
+    value: 'Women Clothing',
+    label: 'Women Clothing',
+  },
+  {
+    value: 'Jewelry',
+    label: 'Jewelry',
+  },
+  {
+    value: 'Ladies Bag',
+    label: 'Ladies Bag',
+  },
+  {
+    value: 'Peshawari Chappal',
+    label: 'Peshawari Chappal',
+  },
+  {
+    value: 'Home Decor',
+    label: 'Home Decor',
+  },
+];
+
 
 const styles = theme => ({
   root: {
@@ -37,7 +68,7 @@ class CustomizedTable extends React.Component {
   componentWillMount(){
     var details = {
       'token':this.state.t,
-      'type':this.state.type,
+      'type':'Men Clothing',
   };
     var formBody = [];
     for (var property in details) {
@@ -79,18 +110,81 @@ class CustomizedTable extends React.Component {
     }
 };
 
+changeType = e => {
+  this.setState({
+    type: e.target.value
+  });
+
+  var details = {
+    'token':this.state.t,
+    'type':this.state.type,
+};
+  var formBody = [];
+  for (var property in details) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+  
+  fetch('/head/findbytype', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' 
+    },
+    body: formBody
+  })
+  .then(res=>res.json())
+  .then(res=>{
+    console.log("we are in this function");
+    console.log(this.state.t);
+    if(res){
+     console.log(res);
+     this.setState({
+       data:res
+     })
+      console.log("After function");
+      console.log(this.state.t);
+    };
+  }
+  );
+
+
+}
+
   render() {
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
-      <Typography variant="display2"> All Sales</Typography>
+      <TextField
+          id="type"
+          select
+          className={classes.textField}
+          value={this.state.type}
+          onChange={e=>this.changeType(e)}
+          SelectProps={{
+            native: true,
+            MenuProps: {
+              className: classes.menu,
+            },
+          }}
+          helperText="Please select your type"
+          margin="normal"
+          >
+          {dropdowntypes.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+          </TextField>
+      <Typography variant="display2"> Items by category</Typography>
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <CustomTableCell>Products</CustomTableCell>
-              <CustomTableCell>Total</CustomTableCell>
-              <CustomTableCell >Shop</CustomTableCell>
-              <CustomTableCell >Date</CustomTableCell>
+              <CustomTableCell>Product Name</CustomTableCell>
+              <CustomTableCell>Price</CustomTableCell>
+              <CustomTableCell >Date Added </CustomTableCell>
+              <CustomTableCell >Size</CustomTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -98,31 +192,14 @@ class CustomizedTable extends React.Component {
                Object.values(this.state.data).map((type,index) => {
                  return (
                   <TableRow className={classes.row} key={index}>
-                    <CustomTableCell>
-                        {
-                          type.products.map((item)=>{
-                            return(item.item_name)+"," 
-                          })
-                          }
-                      </CustomTableCell> 
-                    <CustomTableCell>{type.total}</CustomTableCell>
-                    <CustomTableCell numeric> {type.shop} </CustomTableCell>
-                    <CustomTableCell numeric> {type.date_sale} </CustomTableCell>
+                    <CustomTableCell>{type.item_name}</CustomTableCell>
+                    <CustomTableCell numeric> {type.price} </CustomTableCell>
+                    <CustomTableCell numeric> {type.date_added} </CustomTableCell>
+                    <CustomTableCell numeric> {type.size} </CustomTableCell>
                   </TableRow>
                 );
               })
             }
-            {/* {data.map(n => {
-              return (
-                <TableRow className={classes.row} key={n.id}>
-                  <CustomTableCell>{n.name}</CustomTableCell>
-                  <CustomTableCell numeric>{n.calories}</CustomTableCell>
-                  <CustomTableCell numeric>{n.fat}</CustomTableCell>
-                  <CustomTableCell numeric>{n.carbs}</CustomTableCell>
-                  <CustomTableCell numeric>{n.protein}</CustomTableCell>
-                </TableRow>
-              );
-            })} */}
           </TableBody>
         </Table>
       </Paper>
