0acc8b8656c215771deefd52242c89c728be64a2
Faizan <mirzafaizanejaz@gmail.com>
2018-08-02 10:48:32 +0500

    main warehouse features completed

--- client/src/warehouse/Drawer.jsx
+++ client/src/warehouse/Drawer.jsx
@@ -146,13 +146,6 @@ class ResponsiveDrawer extends React.Component {
     console.log("ADd item on click")
   }
 
-  SalesHandleClick = () => {
-
-    this.setState({
-        OnDisplay:<ViewItems token={this.state.t} handleopen={this.handleClickDialogOpen} handleError={this.handleClickerrorDialogOpen}/>
-    })
-    console.log("View All item Click")
-  }
   PurchaseHandleClick = () => {
 
     this.setState({
@@ -164,20 +157,6 @@ class ResponsiveDrawer extends React.Component {
 
     this.setState({
         OnDisplay:<StockByCategory token={this.state.t}/>
-    })
-    console.log("View All item Click")
-  }
-  OutletHistoryHandleClick = () => {
-
-    this.setState({
-        OnDisplay:<ViewItems token={this.state.t} handleopen={this.handleClickDialogOpen} handleError={this.handleClickerrorDialogOpen}/>
-    })
-    console.log("View All item Click")
-  }
-  SettingsHandleClick = () => {
-
-    this.setState({
-        OnDisplay:<ViewItems token={this.state.t} handleopen={this.handleClickDialogOpen} handleError={this.handleClickerrorDialogOpen}/>
     })
     console.log("View All item Click")
   }
@@ -218,19 +197,12 @@ class ResponsiveDrawer extends React.Component {
             <Divider />
             <ListItem><Button onClick={this.batchitemsshowHandleClick.bind(this)} >Batch Items Show</Button></ListItem>
             <Divider />
-            <ListItem><Button onClick={this.SalesHandleClick.bind(this)}>Sales</Button></ListItem>
-            <Divider/>
             <ListItem><Button onClick={this.PurchaseHandleClick.bind(this)}>Purchase</Button></ListItem>
             <Divider/>
             <ListItem><Button onClick={this.StockbyCategoryHandleClick.bind(this)}>Stock by Category</Button></ListItem>
             <Divider/>
-            <ListItem><Button onClick={this.OutletHistoryHandleClick.bind(this)}>Outlet History</Button></ListItem>
-            <Divider/>
-            <ListItem><Button onClick={this.SettingsHandleClick.bind(this)}>Setting</Button></ListItem>
-            <Divider/>
             <ListItem><Button onClick={this.props.logoutScreen}>Logout</Button></ListItem>
             <Divider />
-           {/*<ListItem><Button onClick={this.ViewItemsHandleClick.bind(this)}>View Items</Button></ListItem>*/} 
             </MuiThemeProvider>
             </Typography>
         </List>
