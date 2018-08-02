441234dde5235ed9095232c88bb82ce401c6306d
Faizan <mirzafaizanejaz@gmail.com>
2018-08-02 11:14:03 +0500

    added search feature in application warehouse

--- client/src/warehouse/FullTable.jsx
+++ client/src/warehouse/FullTable.jsx
@@ -5,6 +5,7 @@ import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Ta
 import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
+import TextField from 'material-ui/TextField';
 
 const CustomTableCell = withStyles(theme => ({
   head: {
@@ -161,12 +162,79 @@ class CustomizedTable extends React.Component {
 
    this.deleteClick =  this.deleteClick.bind(this);
   }
+
+
+  //search feature
+
+  changeItemName = e => {
+    this.setState({
+      itemName:e.target.value,
+    })
+  }
+
+  findItem = () => {
+    let temp = []
+    Object.values(this.state.data).map((type,index)=>{
+      if (type.item_name.toLocaleLowerCase().indexOf(this.state.itemName.toLocaleLowerCase())>=0){
+       temp.push(type);
+      }
+    })
+    this.setState({
+      data:temp
+    });
+  }
+
+  cancelSearch = () => {
+    var details = {
+      'token':this.state.t
+  };
+  
+ 
+  var formBody = [];
+  for (var property in details) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+  
+  
+  fetch('/admin/ShowArticles', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' 
+    },
+    body: formBody
+  })
+  .then(res=>res.json())
+  .then(res=>{
+ 
+    if(res){
+     this.setState({
+       data:res
+     })
+    };
+  }
+  );   
+  }
+
   render() {
     const { classes } = this.props;
 
   return (
     <Paper className={classes.root}>
     {/*<Search/>*/}
+    <TextField
+                    id="itemname"
+                    label="Find Item"
+                    value={this.state.itemName}
+                    placeholder="Find Item"
+                    onChange={e => this.changeItemName(e)}
+                    className={classes.textField}
+                    margin="normal"
+                />
+                <Button  color="primary" onClick={this.findItem}  className={classes.button}>Find</Button>
+                <Button  color="primary" onClick={this.cancelSearch}  className={classes.button}>All Items</Button>
       <Table className={classes.table}>
         <TableHead className={classes.tablehead}>
           <TableRow>
